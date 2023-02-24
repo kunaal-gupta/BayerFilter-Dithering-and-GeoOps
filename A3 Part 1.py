20a88fd9fa451faf07ce9a5d1abec0835d984dc2
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2023-02-23 21:46:00 -0700

    plotted all three output imagese

--- A3 Part 1.py
+++ A3 Part 1.py
@@ -24,7 +24,7 @@ def part1():
 
     for row in range(0, h, 4):  # loop step is 4 since our mask size is 4.
         for col in range(0, w, 4):  # loop step is 4 since our mask size is 4.
-            # TODO: compute pixel value for each location where mask is unshaded (0)
+
             # interpolate each pixel using its every valid (shaded) neighbour
             IG[row, col + 1] = (int(img[row, col]) + int(img[row, col + 2]) + int(img[row + 1, col + 1])) / 3
             IG[row, col + 3] = (int(img[row, col + 2]) + int(img[row + 1, col + 3])) / 2
@@ -42,18 +42,10 @@ def part1():
             IG[row + 3, col + 2] = (int(img[row + 3, col + 1]) + int(img[row + 2, col + 2]) + int(
                 img[row + 3, col + 3])) / 3
 
-    # TODO: show green (IR) in first subplot (221) and add title - refer to rgb one for hint on plotting
-    # plt.figure(figsize=(10, 8))
-    # plt.imshow(IG, cmap='gray')
-    # plt.show()
-    # ...
-    #
-    #     # TODO: reconstruction of the red channel IR (simiar to loops above),
     IR = np.copy(img)  # copy the image into each channel
 
     for row in range(0, h, 4):  # loop step is 4 since our mask size is 4.
         for col in range(0, w, 4):  # loop step is 4 since our mask size is 4.
-            # TODO: compute pixel value for each location where mask is unshaded (0)
 
             # First Row
             IR[row, col + 2] = (int(img[row, col + 1]) + int(img[row, col + 3])) / 2
@@ -68,75 +60,61 @@ def part1():
 
             # Third Row
             IR[row + 2, col] = (int(img[row + 2, col + 1]))
-            IR[row + 2, col+2] = (int(img[row + 2, col + 1]) + int(img[row + 2, col + 3])) / 2
+            IR[row + 2, col + 2] = (int(img[row + 2, col + 1]) + int(img[row + 2, col + 3])) / 2
 
             # Fourth Row
             IR[row + 3, col] = (int(img[row + 2, col + 1]))
-            IR[row + 3, col+1] = (int(img[row + 2, col + 1]))
-            IR[row + 3, col+2] = (int(img[row + 2, col + 2]))
-            IR[row + 3, col+3] = (int(img[row + 2, col + 3]))
+            IR[row + 3, col + 1] = (int(img[row + 2, col + 1]))
+            IR[row + 3, col + 2] = (int(img[row + 2, col + 2]))
+            IR[row + 3, col + 3] = (int(img[row + 2, col + 3]))
 
-    # plt.figure(figsize=(10, 8))
-    # plt.imshow(IR, cmap='gray')
-    # plt.show()
 
     IB = np.copy(img)  # copy the image into each channel
 
     for row in range(0, h, 4):  # loop step is 4 since our mask size is 4.
         for col in range(0, w, 4):  # loop step is 4 since our mask size is 4.
-            # TODO: compute pixel value for each location where mask is unshaded (0)
 
             # Fourth Row
-            IB[row+3, col + 1] = (int(img[row+3, col]) + int(img[row +3 , col + 2])) / 2
-            IB[row+3, col+3] = (int(img[row+3, col + 2]))
+            IB[row + 3, col + 1] = (int(img[row + 3, col]) + int(img[row + 3, col + 2])) / 2
+            IB[row + 3, col + 3] = (int(img[row + 3, col + 2]))
 
             # Third Row
-            IB[row + 2, col] = (int(img[row+1, col]) + int(img[row + 3, col])) / 2
-            IB[row + 2, col + 1] = (int(img[row+1, col]) + int(img[row+1, col + 2]) + int(img[row + 3, col]) + int(
+            IB[row + 2, col] = (int(img[row + 1, col]) + int(img[row + 3, col])) / 2
+            IB[row + 2, col + 1] = (int(img[row + 1, col]) + int(img[row + 1, col + 2]) + int(img[row + 3, col]) + int(
                 img[row + 3, col + 2])) / 4
             IB[row + 2, col + 2] = (int(img[row + 1, col + 2]) + int(img[row + 3, col + 2])) / 2
             IB[row + 2, col + 3] = (int(img[row + 2, col + 2]))
 
             # Second Row
-            IB[row + 1, col+1] = (int(img[row + 1, col]) + int(img[row + 1, col+2]))/2
-            IB[row + 1, col+3] = (int(img[row + 1, col + 2]))
+            IB[row + 1, col + 1] = (int(img[row + 1, col]) + int(img[row + 1, col + 2])) / 2
+            IB[row + 1, col + 3] = (int(img[row + 1, col + 2]))
 
             # First Row
             IB[row, col] = (int(img[row + 1, col]))
-            IB[row, col+1] = (int(img[row + 1, col + 1]))
-            IB[row, col+2] = (int(img[row + 1, col + 2]))
-            IB[row, col+3] = (int(img[row + 1, col + 3]))
-
-
-    # plt.figure(figsize=(10, 8))
-    # plt.imshow(IB, cmap='gray')
-    # plt.show()
+            IB[row, col + 1] = (int(img[row + 1, col + 1]))
+            IB[row, col + 2] = (int(img[row + 1, col + 2]))
+            IB[row, col + 3] = (int(img[row + 1, col + 3]))
 
     rgb = np.dstack((IR, IG, IB))
-    plt.figure(figsize=(10, 8))
+
+    plt.subplot(2, 2, 1)
+    plt.imshow(IG, cmap='gray')
+    plt.title('IG')
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(IR, cmap='gray', )
+    plt.title('IR')
+
+    plt.subplot(2, 2, 3)
+    plt.imshow(IB, cmap='gray')
+    plt.title('IB')
+
+    plt.subplot(2, 2, 4)
     plt.imshow(rgb, cmap='gray')
+    plt.title('rgb')
+
     plt.show()
 
 
-
-#     #
-#     # TODO: show IR in second subplot (224) and title
-#     # ...
-#
-#     # TODO: reconstruction of the blue channel IB (similar to loops above),
-#     # ...
-#     # TODO: show IB in third subplot () and title
-#     # ...
-#
-#     # TODO: merge the three channels IG, IB, IR in the correct order
-#     rgb[:, :, 1] = IG
-#     # ...
-#
-#     # TODO: show rgb image in final subplot (224) and add title
-#     plt.subplot(224)
-#     plt.imshow(rgb), plt.title('rgb')
-#     plt.show()
-#
-#
-# if __name__ == "__main__":
-part1()
+if __name__ == "__main__":
+    part1()
